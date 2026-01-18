1df64817d7d371661c2ace6fced4c839f3a4de95
Andrei Sergheev <chaos95kill@gmail.com>
2026-01-18 20:20:30 +0200

    Add modified with classes

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from "copy-webpack-plugin";
+// import glob from 'glob';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devServer: {
+        static: './dist',
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "templates", to: "templates" },
+                { from: "static/fonts", to: "fonts" },
+                { from: "static/images", to: "images" },
+            ],
+        }),
+    ],
+};
